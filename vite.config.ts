fa9746bfbdef7c6cf46d76eab7937435b1e722d4
vachelfun <vachelfun@gmail.com>
2023-03-10 23:45:28 +0800

    feat: 模板项目

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
+import { fileURLToPath } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: ['.ts', '.tsx', '.json', '.js'],
+  },
+  plugins: [vue(), eslint()],
+})
